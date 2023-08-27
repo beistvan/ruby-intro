6b4c879a662f26d14c8054ede6df4d2d6811aa05
Beregszászi István <beistvan@gmail.com>
2023-08-27 06:28:00 +0200

    Arithmetics

---
+++ arithmetics.rb
@@ -0,0 +1,18 @@
+x = 12
+y = 7
+
+# addition
+puts x + y    # 19
+
+# subtraction
+puts x - y    # 5
+
+# multiplication
+puts x * y    # 84
+
+# division
+puts x/y      # 1
+
+x = 7.0
+y = 2
+puts x/y      # 3.5
