da77e6019e8119d3d7c9b31af01233d8681364a9
Beregszászi István <beistvan@gmail.com>
2023-08-27 06:06:39 +0200

    Variables

---
+++ variables.rb
@@ -0,0 +1,31 @@
+x = 26
+puts x # outputs 26
+
+first = 15
+puts first
+
+x = 29        # integer
+y = 3.14      # float
+s = "String"  # string
+b = true      # boolean
+
+puts x
+puts y
+puts s
+puts b
+
+million = 1_000_000
+puts million
+
+puts 11.class
+puts (22.33).class
+puts true.class
+puts false.class
+puts nil.class
+
+dynamic_variable = 789
+puts dynamic_variable
+dynamic_variable = 123.456
+dynamic_variable = true
+dynamic_variable = "dynamic variable"
+puts dynamic_variable
