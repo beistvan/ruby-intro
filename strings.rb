dca63a87f58e0b0f04ac4d5c5305980fa65675b4
Beregszászi István <beistvan@gmail.com>
2023-08-28 13:20:32 +0200

    Strings

---
+++ strings.rb
@@ -0,0 +1,16 @@
+text1 = "proper text"
+text2 = 'proper text too'
+#text3 = 'improper text is here" # error
+text4 = "the 'text' is here" 
+text5 = "more \"text\" with quotes"
+text6 = "new\nline"
+
+puts text1 
+puts text2
+puts text4
+puts text5
+puts text6
+
+age = 33
+puts "His age is #{age}"
+puts "Next year he will #{age + 1}"
