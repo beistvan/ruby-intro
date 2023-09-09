7712500477ebe7ecfb1ddcb6907b9283475bb368
beistvan <beistvan@gmail.com>
2023-09-09 15:02:26 +0200

    Break

---
+++ break.rb
@@ -0,0 +1,24 @@
+for i in 1..5
+  if i > 3
+    break
+  end
+  puts i
+end
+
+# prints:
+# 1
+# 2
+# 3
+
+x = 10
+while x < 14
+  if x == 12
+    break
+  end
+  puts x
+  x += 1
+end
+
+# prints:
+# 10
+# 11
