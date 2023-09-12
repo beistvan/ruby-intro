b24cced8668707084ab7a3bfdb51931f5bb03691
beistvan <beistvan@gmail.com>
2023-09-12 19:27:50 +0200

    Array - Adding Elements

---
+++ array-adding-elements.rb
@@ -0,0 +1,11 @@
+cities = ["Rome", "London", "Oslo"]
+print cities
+puts 
+cities.insert(1, "Beijing")
+print cities
+puts
+cities.push("Tokyo")
+print cities
+puts
+cities.unshift("Paris")
+print cities
