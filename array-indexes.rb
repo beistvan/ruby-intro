426aad2092e92fd3576aa150eadc252fe6727b94
beistvan <beistvan@gmail.com>
2023-09-12 19:02:29 +0200

    Array's Indexes

---
+++ array-indexes.rb
@@ -0,0 +1,10 @@
+items1 = ["Lenovo", "Asus", "Sony", "Samsung"]
+puts items1[0] # Lenovo
+
+items2 = ["Lenovo", "Asus", "Sony", "Samsung"]
+items2[2] = "Apple"
+print items2
+puts
+
+items3 = ["Lenovo", "Asus", "Sony", "Samsung"]
+puts items3[-1]
