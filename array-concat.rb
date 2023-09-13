196d8594bd95a7735fb91242d1ae7c1a3ad37733
beistvan <beistvan@gmail.com>
2023-09-13 12:46:38 +0200

    Array Concat

---
+++ array-concat.rb
@@ -0,0 +1,9 @@
+city1 = ["Paris", "Tokyo", "Oslo"]
+city2 = ["Paris", "London"]
+
+concat_operator = city1 + city2
+print concat_operator
+puts
+
+concat_method = city1.concat(city2)
+print concat_method
