4708d666ca4b2f7ccfe336c68284b223000a5981
Beregszászi István <beistvan@gmail.com>
2023-12-03 19:03:20 +0100

    Class Truck With Access Methods For Class Variables

---
+++ class-truck-with-access-methods.rb
@@ -0,0 +1,19 @@
+class Truck
+  @@wheels = 6
+  @@color = "white"
+
+  def wheels
+    @@wheels
+  end
+  def wheels=(wheels)
+    @@wheels = wheels
+  end
+    
+  def color
+    @@color
+  end
+  def color=(color)
+    @@color = color
+  end
+
+end
