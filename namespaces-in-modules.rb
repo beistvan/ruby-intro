b338432bb645bd55f3c1b639b59dd0c6e8ad379a
Beregszászi István <beistvan@gmail.com>
2023-12-08 00:03:48 +0100

    Namespaces In Modules

---
+++ namespaces-in-modules.rb
@@ -0,0 +1,21 @@
+module Samsung
+  class Laptop
+    def name
+      puts "Galaxy Book 2"
+    end
+  end
+
+  class Phone
+    def name
+      puts "Galaxy S24"
+    end
+  end
+end
+
+module Apple
+  class Laptop
+    def name
+      puts "MacBook Pro 14"
+    end
+  end
+end
