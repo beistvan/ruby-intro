2328215c3fd9db33331bb1e0f1196edd9024d9b3
Beregszászi István <beistvan@gmail.com>
2023-12-11 20:05:57 +0100

    Two Class Methods For Arithmetic Class: Info And Plus

---
+++ two-class-methods-for-arithmetic.rb
@@ -0,0 +1,11 @@
+class Arithmetic
+    def self.info
+        puts "Class Of Arithmetic"
+    end
+    def self.plus(x, y)
+        puts x + y
+    end
+end
+
+Arithmetic.info # Class Of Arithmetic
+Arithmetic.plus(2, 5) # 7
