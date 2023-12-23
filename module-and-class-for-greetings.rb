9c3b901754f55d89a9623d93a577f32de299955d
Beregszászi István <beistvan@gmail.com>
2023-12-23 14:52:52 +0100

    Module And Class For Greetings

---
+++ module-and-class-for-greetings.rb
@@ -0,0 +1,17 @@
+module Greetings_module
+  def cow
+    puts "Moo!"
+  end
+  def cat
+    puts "Miaw!"
+  end
+end
+
+class Greetings_class
+  def cow
+    puts "Moo!"
+  end
+  def cat
+    puts "Miaw!"
+  end
+end
