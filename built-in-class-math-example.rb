401eb1a536285ff48197095c721bfd3707f8ed27
Beregszászi István <beistvan@gmail.com>
2023-12-24 11:03:02 +0100

    Built-In Class Math - Example

---
+++ built-in-class-math-example.rb
@@ -0,0 +1,12 @@
+# constants
+puts Math::PI        #  3.141592653589793
+puts Math::E         #  2.718281828459045
+
+# square root
+puts Math.sqrt(16)   #  4.0
+
+# cubic root
+puts Math.cbrt(27)   #  3.0
+
+# logarithm with base 10
+puts Math.log10(10.0)#  1.0
